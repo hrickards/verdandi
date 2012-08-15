d9601493ba55c83f1c6977c6edf14303fed491b3
Harry Rickards <hrickards@gmail.com>
2012-08-15 16:47:46 +0100

    Configure mongo for heroku mongohq

--- init.rb
+++ init.rb
@@ -14,6 +14,9 @@ include Verdandi
 
 
 # Database config
-Mongomatic.db = Mongo::Connection.new.db "verdandi"
-uri = URI.parse(ENV["REDISTOGO_URL"] || 'redis://localhost:6379/')
-REDIS = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
+mongo_uri = URI.parse(ENV['MONGOHQ_URL'] || 'mongodb://localhost:27017/verdandi')
+Mongomatic.db = Mongo::Connection.new(mongo_uri.host, mongo_uri.port).db(mongo_uri.path.gsub /^\//, '')
+Mongomatic.db.authenticate mongo_uri.user, mongo_uri.password if mongo_uri.user and mongo_uri.password
+
+redis_uri = URI.parse(ENV["REDISTOGO_URL"] || 'redis://localhost:6379/')
+REDIS = Redis.new :host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password
